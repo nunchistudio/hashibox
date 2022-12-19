6831651a26412cf7ea066edac1fdad3fad6f850c
Loïc Saint-Roch <loic@nunchi.studio>
2022-12-19 18:54:39 +0100

    version(nomad): Update to v1.4.3
    Signed-off-by: Loïc Saint-Roch <loic@nunchi.studio>

--- modules/nomad/tasks/update.sh
+++ modules/nomad/tasks/update.sh
@@ -7,7 +7,7 @@ source /hashibox/.env
 
 # Set Nomad version.
 CNI_PLUGINS_VERSION="1.0.1"
-NOMAD_VERSION="1.4.0"
+NOMAD_VERSION="1.4.3"
 if [[ ! -z ${NOMAD_LICENSE} ]]; then
   NOMAD_VERSION+="+ent"
 fi
